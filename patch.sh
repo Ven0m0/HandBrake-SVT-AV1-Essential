f91dec99f6a4b6df2fe903d68ee42316dc18d463
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-12-28 19:58:29 +0100

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 # shellcheck enable=all shell=bash source-path=SCRIPTDIR
-
 BASEDIR=$(dirname "$0")
 src_dir="HandBrake"
 help="Usage: patch.sh [src_dir=\"HandBrake\"] [options]
@@ -11,49 +10,22 @@ The src_dir is the directory that contains the HandBrake source code (defaults t
 
 If no directory is found, the program exits"
 
-if [ "$#" -gt 2 ]; then
-    echo "$help"
-	exit 1
-fi
-
-
+[[ "$#" -gt 2 ]] && { echo "$help"; exit 1; }
 for (( i=1; i <= "$#"; i++ )); do
-	case ${!i} in
-		-h | --help)
-			echo "$help"
-			exit 1
-			;;
-		-c | --clone)
-			if [ "$i" -lt "$#" ]; then
-				echo "$help"
-				exit 1
-			fi
-			rm -rf HandBrake
-			git clone https://github.com/HandBrake/HandBrake.git
-			;;
-		-*)
-			echo "${!i} option doesn't exists!"
-			echo "$help"
-			exit 1
-			;;
-		*)
-			src_dir=$1
-			;;
+  case ${!i} in
+	-h | --help) echo "$help"; exit 1 ;;
+	-c | --clone)
+		[[ "$i" -lt "$#" ]] && { echo "$help"; exit 1; }
+		rm -rf HandBrake; git clone --depth 1 https://github.com/HandBrake/HandBrake.git ;;
+	-*) echo "${!i} option doesn't exists!"; echo "$help"; exit 1 ;;
+	*) src_dir="$1" ;;
 	esac
 done
-
-if [ ! -d "$src_dir" ]; then
-	echo "$src_dir directory doesn't exists!"
-	echo "$help"
-	exit 1
-fi
-
-
-for filename in $BASEDIR/patches/*.patch; do
-    patch -t -N -p1 -d $src_dir < "$filename"  || exit 1
+[[ ! -d "$src_dir" ]] && { echo "$src_dir directory doesn't exists!"; echo "$help"; exit 1; }
+for filename in ${BASEDIR}/patches/*.patch; do
+  patch -t -N -p1 -d "$src_dir" < "$filename"  || exit 1
 done
-
 # The flatpak build refers to the latest commit, so we add a commit that includes the patches
-cd $src_dir
+cd "$src_dir"
 git add .
 git -c user.name='deadbeef' -c user.email='deadbeef' commit -m "Patch"
